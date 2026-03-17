c941fcbab6feb3ca099a9fe56ab3202565fa47fb
Babbelox <gabriele.garzillo@mail.polimi.it>
2026-03-17 23:51:29 +0100

    aggiunta la get dei preferiti

---
+++ api/utente/campi a 5/preferiti/route.ts
@@ -0,0 +1,57 @@
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/authConfig"
+// Funzione che prende campi DISPONIBILI a 5
+
+export async function GET(request: Request){
+
+    const session = await getServerSession(authOptions)
+    if (!session || !session.user) {
+        return NextResponse.json(
+            { message: "Utente non autenticato" },
+            { status: 401 }
+        )
+    }
+    
+    const id_utente = session.user.id
+
+    try {
+        
+        const preferiti = await prisma.preferiti.findMany({
+        where: {id_utente: parseInt(id_utente)},
+        take: 15
+    })
+
+        if(preferiti.length === 0){
+            return NextResponse.json(
+                {message: "Nessun campo preferito"},
+                {status: 200}
+            )
+        }
+
+        // Restiuiamo proprio i campi preferiti
+
+        const campo = await prisma.tabella_campi.findMany({
+            where: {
+                id_campo: {
+                    in: preferiti.map(p => p.id_campo)
+                }
+            }
+        })
+        
+        return NextResponse.json(
+            {message: "campi trovati", campo},
+            {status: 200}
+        )
+
+}
+
+    catch {
+        return NextResponse.json(
+            {message: "Errore prisma"},
+            {status: 500}
+        )
+    }
+
+}
